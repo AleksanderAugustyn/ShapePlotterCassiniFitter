2a91e25c513286939fe7d51c185cbab50ab63c3e
Aleksander Augustyn <aleksander.augustyn@ncbj.gov.pl>
2025-01-20 19:46:06 +0100

    refactor: Enhance plot save functionality with higher DPI output (master)
    Increase image resolution by setting DPI to 600 for better quality exports.
    
    This change improves the output quality of saved Cassini shape plots.
    
    Signed-off-by: Aleksander Augustyn <aleksander.augustyn@ncbj.gov.pl>

--- ShapePlotterCassiniFitter.py
+++ ShapePlotterCassiniFitter.py
@@ -592,7 +592,7 @@ class CassiniShapePlotter:
         number_of_neutrons = int(self.slider_n.val)
         params = [self.slider_alpha.val] + [s.val for s in self.sliders]
         filename = f"cassini_shape_{number_of_protons}_{number_of_neutrons}_{'_'.join(f'{p:.2f}' for p in params)}.png"
-        self.fig.savefig(filename)
+        self.fig.savefig(filename, dpi=600)
         print(f"Plot saved as {filename}")
 
     def update_plot(self, _):
